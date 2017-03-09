cd56c22f21145630d14fb5b9bda5d8385b8f3adb
Nyks45 <winglow45@gmail.com>
2017-03-09 23:36:52 +0300

    From The Ashes We Born

--- core/product_config.mk
+++ core/product_config.mk
@@ -179,12 +179,12 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-# A CM build needs only the CM product makefiles.
-ifneq ($(CM_BUILD),)
-  all_product_configs := $(shell find device -path "*/$(CM_BUILD)/lineage.mk")
+# A ONEUI build needs only the ONEUI product makefiles.
+ifneq ($(ONEUI_BUILD),)
+  all_product_configs := $(shell find device -path "*/$(ONEUI_BUILD)/oneui.mk")
   ifeq ($(all_product_configs),)
-    # Fall back to cm.mk
-    all_product_configs := $(shell find device -path "*/$(CM_BUILD)/cm.mk")
+    # Fall back to oneui.mk
+    all_product_configs := $(shell find device -path "*/$(ONEUI_BUILD)/oneui.mk")
   endif
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
@@ -196,9 +196,9 @@ else
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
   endif # TARGET_BUILD_APPS
-endif # CM_BUILD
+endif # ONEUI_BUILD
 
-ifeq ($(CM_BUILD),)
+ifeq ($(ONEUI_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
